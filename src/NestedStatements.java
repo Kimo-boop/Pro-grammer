f2b2181c40ebda3f0e606356651258f1dd4d5a6f
Kimo-boop <69760092+Kimo-boop@users.noreply.github.com>
2022-11-12 16:46:57 +0100

    Update NestedStatements.java

--- src/NestedStatements.java
+++ src/NestedStatements.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class NestedStatements {
+    public static void main(String[]args) {
+        System.out.print("Input your age:");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int age = Integer.parseInt(s);
+        if (age >= 18) {
+            System.out.println("Input your fav food");
+            String food = sc.nextLine();
+            if (food.equals("Pizza")) {
+                System.out.println("mine too");
+            } else {
+                System.out.println("not mine");
+            }
+        }
+
+
+        else if (age >= 13){
+            System.out.println("You're a teenager");
+        }
+        else {
+            System.out.println ("You're not a teenager or an adult");
+        }
+
+    }
 }
