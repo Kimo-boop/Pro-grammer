ca0720b7cfce48ff1521b2beed259ccad51dad8f
Kimo-boop <69760092+Kimo-boop@users.noreply.github.com>
2022-11-13 10:26:45 +0100

    array und so

---
+++ src/arrays.java
@@ -0,0 +1,15 @@
+public class arrays {
+
+
+    public static void main (String[]args) {
+     String[] newARR = new String[5];
+     newARR[0] = "hello";
+     newARR[1] = "hi";
+     newARR[2] = "tim";
+     newARR[3] = "bill";
+     newARR[4] = "joe";
+     String x= newARR [4];
+     System.out.println(x);
+
+    }
+}
